4e2f21f9d79291c37f4868f8e46c1d7b61ba4ded
Steven Samy <stevensivo27@gmail.com>
2022-01-08 21:03:40 +0200

    #

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django import http
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+def about(request):
+    pass   
