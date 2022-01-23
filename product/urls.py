37b7e1cdb20ba2b8cd66b37989e22a2a74247981
Steven Samy <stevensivo27@gmail.com>
2022-01-23 21:38:31 +0200

    DB

---
+++ product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('product', views.product , name = 'product' ),
+    path('', views.products, name = ' products' ) 
+
+
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product(request):
+    return render(request, 'product/product.html')
+
+def products(request):
+    return render(request, 'product/products.html', {'name':'ahmed'})
