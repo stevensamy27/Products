cd756c151bb5243c931f87df1f0a043e0612d6a9
Steven Samy <stevensivo27@gmail.com>
2022-02-01 21:49:09 +0200

    qery set

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from os import name
 from re import X
+from unicodedata import category
 from django.shortcuts import render
 from .models import Product
 
@@ -8,5 +9,28 @@ def product(request):
     return render(request, 'product/product.html')
 
 def products(request):
-    x = {'pro':Product.objects.all().filter(name = 'apple 123')}
+    a = Product.objects.all()
+    x = {'pro':a.filter(name__contains = 'o')}
     return render(request, 'product/products.html',x )
+
+
+
+    ### some query set
+    '''
+    .all() >> show all products
+    .filter() >> filter products by
+    .order_by() >> To order Products by
+    .count >> to count how many product on site  ## must write on string like [ str(a.count()) ]
+    .exclude() show all products excluded some products
+
+    '''
+
+    ### some ranges in .filter() query set
+    '''
+    (name__exact = ' HE5O ')
+    (name__contains = ' o ')
+    (price__in = [0 , 10])
+    (price__range = [0 , 10])
+
+
+    '''
