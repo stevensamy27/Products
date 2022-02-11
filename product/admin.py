0d461e60b0a89994fd6cb88f92a7cfc447ef862d
Steven Samy <stevensivo27@gmail.com>
2022-02-11 23:28:04 +0200

    admin panel

--- product/admin.py
+++ product/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import Product, Test
 # Register your models here.
 
-admin.site.register(Product) 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    list_display_link = ['price']
+    list_editable = ['category']
+    
+
+admin.site.register(Product,  ProductAdmin) 
 admin.site.register(Test)
 admin.site.site_header = 'Nofal'
 admin.site.site_title = 'BigNofal'
